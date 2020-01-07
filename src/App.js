34907e465876baacc249db47419ec2aa356b2517
emsbn <emsbn1@gmail.com>
2020-01-08 00:05:40 +0900

    componentDidCatch

--- src/App.js
+++ src/App.js
@@ -8,15 +8,24 @@ import MyComponent from "./MyComponent";
 // 클래스형
 class App extends Component {
   state = {
-    counter: 1
+    counter: 1,
+    error: false
   };
+
+  componentDidCatch(error, info) {
+    console.log(error);
+    console.log(info);
+    this.setState({
+      error: true
+    });
+    // API를 통해서 서버로 오류 내용 날리기
+  }
   constructor(props) {
     super(props);
     console.log("constructor");
   }
   componentDidMount() {
     console.log("componentDidMount");
-    console.log(this.myDiv.getBoundingClientRect());
   }
   handleClick = () => {
     this.setState({
@@ -25,6 +34,9 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.error) {
+      return <div>에러가 났어요!</div>;
+    }
     return (
       <div ref={ref => (this.myDiv = ref)}>
         {this.state.counter < 10 && <MyComponent value={this.state.counter} />}
